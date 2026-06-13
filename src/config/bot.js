d64ddab10925232203698b29506e4d560984c198
benzinea77-source <benzinea77@gmail.com>
2026-06-13 03:24:52 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "My daddys server https://discord.gg/scythevs",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -136,7 +136,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "Scythe Bot",
       // Footer icon URL (null = no icon).
       icon: null,
     },
