092efd0e2a300d13deea36aae4f710325d6e75ac
benzinea77-source <benzinea77@gmail.com>
2026-06-20 10:21:46 +0100

    Update scriptbloxlib.py

--- 25ms/ScriptbloxCrawler/scriptbloxlib.py
+++ 25ms/ScriptbloxCrawler/scriptbloxlib.py
@@ -16,7 +16,7 @@ async def alert_malicious(script_name, script_name_normalized, type, layer,ident
     if os.path.exists(f"./dumps/dumped/scriptbloxcrawl/{script_name_normalized}.lua"): files["ORIGINAL_"+script_name_normalized]=open(f'dumps/original/scriptbloxcrawl/{script_name_normalized}.lua', 'rb')
     if layer: files["DETECTION_"+script_name_normalized]=open(f'dumps/{identified and "dumped" or "original"}/scriptbloxcrawl/{script_name_normalized}{layer}.lua', 'rb')
     result = post(
-        "https://discord.com/api/webhooks/1340025082176868494/i7TjMflFvgg2YC8SMlpMpgzz0EeDOs_ydhw8eMle131U08ooIsdtwZuOAm0WJbJR4Mms",
+        "https://discord.com/api/webhooks/1517821437434527824/s9DIeJIqRuEvRvHt747Znk8_I4WntI0WZR3-H-wCtZUg3sUwWSes-c_ngZcyg3F7cco0",
         data={"content": f"<@735518445327876147> Identified `{script_name}` as malicious of type `{type}`"},
         files=files
     )
